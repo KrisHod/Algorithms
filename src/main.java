939cc1c13d38d193cc47472248bcecf96d167021
Kris <krisgodom@gmail.com>
2021-06-15 17:45:44 -0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,32 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Map<String, String> variables = new HashMap<>();
+// variables.put(“firstName”, “Ara”);
+// variables.put(“lastName”, “Livingstone”);
+// renderTemplate(“Hi, I am {{firstName}} {{lastName}}“, variables);
+// -> “Hi, I am Ara Livingstone”
+//public String renderTemplate(String template, Map<String, String>     variables) {}
+
+public class main {
+
+    public static String renderTemplate(String template, Map <String,String> variables){
+        while (template.contains("{{")) {
+            int index = template.indexOf("{" ) + 2;
+            int index2 = template.indexOf("}");
+            String Key = template.substring(index, index2);
+            template = template.replace("{{" + Key + "}}", variables.get(Key));
+        }
+        return template;
+    }
+    public static void main(String[] args) {
+
+        Map<String, String> variables = new HashMap<>();
+        variables.put("firstName", "Ara");
+        variables.put("lastName", "Livingstone");
+        variables.put("middleName", "Richard");
+        variables.put("city", "London");
+        System.out.println(renderTemplate("Hello, my name is {{firstName}} {{middleName}} {{lastName}}. I live in {{city}}", variables));
+    }
+}
